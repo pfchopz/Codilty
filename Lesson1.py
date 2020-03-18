c0db13442df34e30e3fcc33e16588d3e1d567640
pfchopz <38539374+pfchopz@users.noreply.github.com>
2020-03-18 15:51:59 -0500

    Test

---
+++ Lesson1.py
@@ -0,0 +1,10 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+Test = 10
+
+def solution(N):
+    # write your code in Python 3.6
+    binary = bin(N)[2:]
+
+print(solution(Test))
