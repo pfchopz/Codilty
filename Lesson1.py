15179c4b3d9b6208b550319418c7005d1878e6ed
pfchopz <38539374+pfchopz@users.noreply.github.com>
2020-03-18 16:14:24 -0500

    Final

--- Lesson1.py
+++ Lesson1.py
@@ -1,10 +1,24 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-Test = 10
+Test = 561892
 
 def solution(N):
     # write your code in Python 3.6
     binary = bin(N)[2:]
+    a = 0
+    b = 0
+
+    for i in binary:
+        if i == '0':
+            a += 1
+        else:
+            if b < a:
+                b = a
+                a = 0
+            else:
+                a = 0
+
+    return b
 
 print(solution(Test))
