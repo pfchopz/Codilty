6f22c5ac70361e0ff66c3040360cc0091b47d875
pfchopz <38539374+pfchopz@users.noreply.github.com>
2020-04-13 11:21:13 -0500

    Final

---
+++ Lesson 6.2.py
@@ -0,0 +1,11 @@
+TestArray = [-5, 5, -5, 4]
+
+def solution(A):
+    # write your code in Python 3.6
+    A.sort()
+    if (A[-2] * A[-3]) < (A[0] * A[1]) and A[-1] > 0:
+        return A[-1] * A[0] * A[1]
+    else:
+        return A[-1] * A[-2] * A[-3]
+
+print(solution(TestArray))
