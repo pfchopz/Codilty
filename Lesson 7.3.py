41cc13cda072a009cd804ac6dcd7e81772d785c7
pfchopz <38539374+pfchopz@users.noreply.github.com>
2020-04-17 14:43:59 -0500

    Final

---
+++ Lesson 7.3.py
@@ -0,0 +1,22 @@
+String = '{[()()]}'
+
+def solution(S):
+    # write your code in Python 3.6
+    dict = {'{': '}', '[': ']', '(': ')'}
+    stack = []
+
+    for i in S:
+        if i in dict:
+            stack.append(dict[i])
+        elif not stack:
+            return 0
+        else:
+            if stack.pop() != i:
+                return 0
+
+    if not stack:
+        return 1
+    else:
+        return 0
+
+print(solution(String))
